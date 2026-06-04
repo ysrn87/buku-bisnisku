fdf4310fc6e27346c98b9c2c3712e05faeaf164c
yusran0102 <yusranmuhammad687@gmail.com>
2026-06-04 21:01:13 +0700

    fix: page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,14 @@
-import { redirect } from "next/navigation";
-export default function Home() { redirect("/login"); }
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, [router]);
+
+  return null;
+}
