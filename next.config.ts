f6f1e30316ffbb7ed4f3507e53081b72cd7a4056
yusran0102 <yusranmuhammad687@gmail.com>
2026-06-04 20:43:57 +0700

    fix: next.config file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
-  allowedDevOrigins: ["192.168.1.16"],
 };
 
 export default nextConfig;
