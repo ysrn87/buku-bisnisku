4d51bb1f2963cdd58393dee7b74f4e3c9b344408
yusran0102 <yusranmuhammad687@gmail.com>
2026-06-04 20:36:23 +0700

    fix: route bugs

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,5 +1,10 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
+      {children}
+    </SessionProvider>
+  );
 }
